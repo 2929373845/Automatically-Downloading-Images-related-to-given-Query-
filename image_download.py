9ec2703cf479b851346018fc9d1b1f3548134047
Kaustubh Devkar <kdevkar1998@gmail.com>
2017-12-24 00:06:32 +0530

    updated ReadMe

--- image_download.py
+++ image_download.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 payload = {'q': 'india', #enter your query here
            'source':'lnms','tbm':'isch'}
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36'}    
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+			AppleWebKit/537.36 (KHTML, like Gecko) \
+			Chrome/59.0.3071.86 Safari/537.36'}    
 s = requests.get('http://www.google.com/search',headers=headers,params=payload).text
 soup = BeautifulSoup(s, 'html.parser')
 i=0
